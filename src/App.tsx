4af1aa3e32ce97b30c7f65ef2800ecf050d853d8
unknown <asankulov-t>
2023-05-24 01:41:03 +0600

    edit main css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Main.css';
+import './main.css';
 import NavBar from "./components/NavBar/NavBar";
 import NavSide from "./components/NavSide/NavSide";
 import ContentContainer from "./components/Containers/ContentContainer";
